9ef619dd1104aa7765948df8eb7c10b348141a91
OkiSenpai <omersatelit@gmail.com>
2025-01-14 09:11:21 +0100

    first ex addded

---
+++ index.py
@@ -0,0 +1,149 @@
+# print("Bonjour")
+# print("welcome to the python training")
+# print("amuser-vous bien")
+
+# a = 5
+# b = 3
+
+# print(a+b)
+
+# nom = "Aslani"
+# Prenom = "Omer"
+# print(nom + " " + Prenom)
+
+# x = 7 
+# y = 8 
+# print(x , y)
+
+# a = 10
+# b = 3
+# print(a / b)
+
+# enterAge = input("quell est votre age? ")
+
+# age = int(enterAge)
+
+# print( "vous avez " ,age , "age" )
+
+
+# c = 3.14
+# print(c, type(c))
+
+# string = "formation python"
+
+# xString = string.upper()
+
+# print(xString)
+
+# userInput = input("ecroivez quelque chose: ")
+# chiffre = len(userInput)
+# print("vous avez ecrit ", chiffre, "lettres")
+
+
+# celcius = input("entrez la temperature en celcius: ")
+# celcius = int(celcius)
+# fahrenheit = (celcius * 9/5) + 32
+# print("la temperature en fahrenheit est: ", fahrenheit)
+
+# numbreOne = float(input("tapper un chiffre "))
+# numbreTo = float(input("tapper un chiffre "))
+
+# print(numbreOne + numbreTo , numbreOne - numbreTo , numbreOne * numbreTo ,numbreOne / numbreTo)
+
+# numbreOne = float(input("tapper un chiffre "))
+# operator = input("tapper + , - , * , ou / ")
+# numbreTo = float(input("tapper un chiffre "))
+# res = eval(f"{numbreOne} {operator} {numbreTo}")
+# if operator == "+":
+#     print(numbreOne + numbreTo)
+# elif operator == "-":
+#     print(numbreOne - numbreTo)
+# elif operator == "*":
+#     print(numbreOne * numbreTo)
+# else:
+#     print(numbreOne / numbreTo)
+
+# numbreOne = float(input("tapper un chiffre "))
+# numbreTo = float(input("tapper un chiffre "))
+
+# if numbreOne > numbreTo:
+#     print(str(numbreOne) + " est plus grand que " + str(numbreTo))
+# elif numbreOne == numbreTo:
+#     print(str(numbreOne) +" est egal que " + str(numbreTo))
+# else:
+#     print(str(numbreOne) + " est plus petit que " + str(numbreTo) )
+  
+# chifre = int(input("tapper un chiffre entier "))
+
+# if chifre % 2 == 0:
+#     print(str(chifre) + " est un chifre pair")
+# else:
+#     print(str(chifre) + " est un chifre inpair")
+
+# chifre = int( input("tapper un chifre negative"))
+# if chifre < 0:
+#     print(chifre * (-1))
+
+# chifre1 = int(input("tapper un chifre "))
+# chifre2 = int(input("tapper un chifre "))
+# chifre3 = int(input("tapper un chifre "))
+
+# if chifre1 > chifre2 and chifre1 > chifre3:
+#     print(str(chifre1) + " est le plus grand")
+# elif chifre2 > chifre1 and chifre2 > chifre3:
+#     print(str(chifre2) + " est le plus grand")
+# elif chifre3 > chifre1 and chifre3 > chifre2:
+#     print(str(chifre3) + " est le plus grand")
+# else:
+#     print("les chife sont egaux")
+
+# prix = int(input("tapper le prix "))
+# prix10 = prix - 10
+# prix5 = prix - 5
+
+# if prix > 100:
+#     print("le prix est avec la reduction de 10 est " + str(prix10))
+# else:
+#     print("le prix evec la reduction de 5 est " + str(prix5))
+
+# ageUtilisateur = int(input("quelle est votre age? "))
+# if ageUtilisateur < 12 and ageUtilisateur > 2:
+#     print("Enfant")
+# elif ageUtilisateur >= 12 and ageUtilisateur <= 17:
+#     print("Ado")
+# elif ageUtilisateur <=2:
+#     print(" you are stupid")
+# else:
+#     print("Adult")
+
+# pas = input("tapper votre mode de pass ")
+
+# if pas == "Python123":
+#     print("Acces autoriser")
+# else:
+#     print("Acces refusé")
+
+
+# chifre = int(input("tapper un chifre"))
+# if chifre % 3 == 0 and chifre % 3 == 0:
+#     print(str(chifre) + " est divisable avec 3 et avec 5")
+# elif chifre % 5 == 0:
+#     print(str(chifre) + " est divisable par 5")
+# elif chifre % 3 == 0:
+#     print(str(chifre) + " est divisable par 3")
+# else:
+#     print(str(chifre) + " n'est pas divisable ni avec 3 ni avec 5")
+
+chifre = 33
+
+devin = int(input("deviner un chifre entre 1 et 50 "))
+
+
+
+while devin != chifre:
+    if devin > chifre:
+     devin = int(input("plus grand , resseyer "))
+    elif devin < chifre:
+       devin = int(input("plus petit, resseyer "))
+    else:
+        print( " vous avez deviné, le chifre est  " + str(chifre)) 
