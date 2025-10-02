7303ca8fdcf0726301f5d1b5204f992fad2b596c
OkiSenpai <omersatelit@gmail.com>
2025-10-02 16:21:39 +0200

    sdada

---
+++ dictioner/tp10.py
@@ -0,0 +1,32 @@
+# personne = {
+#     "nom" : "omer",
+#     "age" : 31,
+#     "ville" : "Bruxellses"
+# }
+
+# personne["tel"] = "046583547"
+# personne["ville"] = "Barcelone"
+
+# if "tel" in personne:
+#     del personne["tel"]
+
+# print(personne.get("note","pas de note")) 
+
+
+# etudiant = {
+#     "nom": "Alice", 
+#     "note": 15, 
+#     "groupe":"A"
+#     }
+
+# for i in etudiant.items():
+#     print(i)
+
+    
+# p = "chocolad"
+
+# c={}
+# for i in p:
+#     c[i]=c.get(i,0)+1
+# print(c)
+
